b6137b490294ec02fd16b5ba87aedea034863329
Kevin-Tamayose <125572668+Kevin-Tamayose@users.noreply.github.com>
2026-03-29 18:39:55 -0300

    Importacao do projeto

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+encoder = joblib.load('Pipeline/encoder.pkl')
+scaler = joblib.load('Pipeline/scaler.pkl')
+kmeans = joblib.load('Pipeline/kmeans.pkl')
+
+st.title('Grupos de interesse para marketing')
+st.write("""
+         Neste projeto, aplicamos o algoritmo de clusterização K-means para identificar e prever agrupamentos de interesses de usuários, com o objetivo de direcionar campanhas de marketing de forma mais eficaz.
+         Através dessa análise, conseguimos segmentar o público em bolhas de interesse, permitindo a criação de campanhas personalizadas e mais assertivas, com base nos padrões de comportamento e preferências de cada grupo.
+         """)
+
+up_file = st.file_uploader('Escolha um arquivo CSV para realizar a previsão',type='csv')
+
+def processar_prever (df):
+    encoded_sexo = encoder.transform(df[['sexo']])
+    encoded_df = pd.DataFrame(encoded_sexo, columns=encoder.get_feature_names_out(['sexo']))
+    dados = pd.concat([df.drop('sexo', axis=1), encoded_df], axis=1)
+        
+    dados_escalados = scaler.transform(dados)
+
+    cluster = kmeans.predict(dados_escalados)
+
+    return cluster
+
+if up_file is not None:
+
+    st.write("""
+                    ### Descrição dos Grupos:
+                    - **Grupo 0** é focado em um público jovem com forte interesse em moda, música e aparência.
+                    - **Grupo 1** está muito associado a esportes, especialmente futebol americano, basquete e atividades culturais como banda e rock.
+                    - **Grupo 2** é mais equilibrado, com interesses em música, dança, e moda.
+                """)
